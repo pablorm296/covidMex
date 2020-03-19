4e02b66d4145eb209f3e983077ac808fc795c561
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-19 16:18:10 -0600

    Hook functions added

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function() {
+  packageStartupMessage("covidMex Package")
+  packageStartupMessage("Versión: 0.2.0")
+  packageStartupMessage("Last Update: 19/03/2020")
+  packageStartupMessage("Available Data Sources:")
+  packageStartupMessage("    Confirmed Cases Table: Serendipia, Guzmart")
+  packageStartupMessage("    Suspect Cases Table: Serendipia")
+}
