8c24efd12bf372408613030f1b40f162ef9f5c83
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-20 23:33:03 -0600

    Fixed onAttach message!

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("covidMex Package")
-  packageStartupMessage("Versión: 0.2.0")
-  packageStartupMessage("Last Update: 19/03/2020")
+  packageStartupMessage("Version: 0.2.0")
+  packageStartupMessage("Last Package Update: 19/03/2020")
   packageStartupMessage("Available Data Sources:")
   packageStartupMessage("    Confirmed Cases Table: Serendipia, Guzmart")
   packageStartupMessage("    Suspect Cases Table: Serendipia")
