70668394d677dc73e73a385e38c00cac302f4980
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-04-26 23:13:33 -0500

    Updated startup message

--- R/startUpMessage.R
+++ R/startUpMessage.R
@@ -4,9 +4,9 @@
 startUpMessage <- function() {
   packageStartupMessage("covidMex Package")
   packageStartupMessage(paste("Version:", packageVersion("covidMex")))
-  packageStartupMessage("Last Package Update: 19/03/2020")
+  packageStartupMessage("Last Package Update: 26/04/2020")
   packageStartupMessage("Available Data Sources:")
-  packageStartupMessage("    Confirmed Cases in Mexico: Serendipia, Guzmart")
+  packageStartupMessage("    Confirmed Cases in Mexico: Serendipia, SSA")
   packageStartupMessage("    Suspect Cases in Mexico: Serendipia")
   packageStartupMessage("    WorldWide Situation Report: ECDC, JHU CSSE")
 }
